08963e2e62525b7270feabea588c78cdd95261f2
erickb21 <alexandre.b@codeur.online>
2018-05-24 10:33:22 +0200

    Init projet

---
+++ php/controleur.php
@@ -0,0 +1,7 @@
+
+<?php
+require("model.php");
+
+foreach ($callBack as $row){
+echo '<p>'.print_r($row).'</p>';
+}
---
+++ rewriting.php
@@ -0,0 +1,15 @@
+<?php
+require("./php/controleur.php");
+?><!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+</head>
+<body>
+hello world
+<?php
+include("./php/controleur.php");
+?>
+</body>
+</html>
