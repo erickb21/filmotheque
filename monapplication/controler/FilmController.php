bdc5cfdc654dbb548fe65432480398d593f3996c
r0m21 <romainwenger21@gmail.com>
2018-05-29 22:20:18 +0200

    Ajout de fonction dans le model pour récupérer le réalisateur et le genre du film séléctionné, modification structure html du template film/display.html.twig, modification du FilmController

--- monapplication/controler/FilmController.php
+++ monapplication/controler/FilmController.php
@@ -3,15 +3,15 @@
 class FilmController extends Controler {
    public function display() {
       $slug = $this->route["params"]["id"];
-      $film = Film::getFromSlug($slug);
-      /* $realisateur = Film::getRealisateur($film['id_FILMS']); */
+      $film = Film::getFilm($slug);
+      $realisateur = Film::getRealisateur($film['id_FILMS']);
+      $genre = Film::getGenre($film['id_FILMS']);
+
       $template = $this->twig->loadTemplate('/film/display.html.twig');
       echo $template->render(array(
         'film' => $film,
-        /* 'realisateur' => $realisateur */
+        'realisateur' => $realisateur,
+        'genre' => $genre
       ));
    }
-
-    
-
 }
